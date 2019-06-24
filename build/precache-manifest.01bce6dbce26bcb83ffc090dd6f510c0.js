7309de0c3fae8b8453259db23650bfa00e79e46b
Daniel Sternlicht <gdblogst@gmail.com>
2019-06-24 15:39:12 +0300

    Updates

---
+++ build/precache-manifest.01bce6dbce26bcb83ffc090dd6f510c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665bf3a984f5b3f53e9a79738c34f3cf",
+    "url": "/react-live-charts/index.html"
+  },
+  {
+    "revision": "18c3dd3193801e306b28",
+    "url": "/react-live-charts/static/css/main.a7910b5e.chunk.css"
+  },
+  {
+    "revision": "3ca974c3c5256a1c413a",
+    "url": "/react-live-charts/static/js/2.8e9ad766.chunk.js"
+  },
+  {
+    "revision": "18c3dd3193801e306b28",
+    "url": "/react-live-charts/static/js/main.0ae89f82.chunk.js"
+  },
+  {
+    "revision": "ce2862b37474267e4cfa",
+    "url": "/react-live-charts/static/js/runtime~main.f618a116.js"
+  }
+]);
